088a600078ad73dee96ecf3a3abad934f54877bc
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2026-02-06 18:09:19 +0100

    fix(GithubIcon): rename GithubIcon to GitHubIcon in export

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,5 @@
 export { default as BaseLink } from './BaseLink';
-export { default as GitHubIcon } from './icons/GithubIcon';
+export { default as GitHubIcon } from './icons/GitHubIcon';
 export { default as LinkedInIcon } from './icons/LinkedInIcon';
 export { default as Hamburger } from './Hamburger';
 export { default as Menu } from './Menu';
