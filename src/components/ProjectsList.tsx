85fe8e43ea548caaf8e4f0dd81613e2fa36ef03f
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2026-02-06 17:39:20 +0100

    refactor(App.tsx): remove unused code

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,36 +1,36 @@
-// import type { ProjectItem } from '../types/main';
-// import data from '../data/data';
-// import { NavigationLink } from '../ui';
-// import type { Data } from '../types/data';
-// import type { JSX } from 'react';
-// import List from '../ui/List';
+import type { ProjectItem } from '../types/main';
+import data from '../data/data';
+import { NavigationLink } from '../ui';
+import type { Data } from '../types/data';
+import type { JSX } from 'react';
+import List from '../ui/List';
 
-// const ProjectsList = (): JSX.Element => {
-//   const projects: Data['projects'] = data.projects;
-//   const root: ProjectItem = projects[0];
-//   const projectsIds = root?.childIds ?? [];
+const ProjectsList = (): JSX.Element => {
+  const projects: Data['projects'] = data.projects;
+  const root: ProjectItem = projects[0];
+  const projectsIds = root?.childIds ?? [];
 
-//   const items = projectsIds
-//     ?.map((id) => {
-//       const projectItem = projects[id];
-//       return projectItem.url ? (
-//         <NavigationLink
-//           key={id}
-//           to={projectItem.url}
-//           value={projectItem.label || 'Untitled'}
-//           className="projects-list__link"
-//         />
-//       ) : null;
-//     })
-//     .filter(Boolean);
+  const items = projectsIds
+    ?.map((id) => {
+      const projectItem = projects[id];
+      return projectItem.url ? (
+        <NavigationLink
+          key={id}
+          to={projectItem.url}
+          value={projectItem.label || 'Untitled'}
+          className="projects-list__link"
+        />
+      ) : null;
+    })
+    .filter(Boolean);
 
-//   return (
-//     <List
-//       items={items}
-//       block="projects-list"
-//       label="projekty"
-//     />
-//   );
-// };
+  return (
+    <List
+      items={items}
+      block="projects-list"
+      label="projekty"
+    />
+  );
+};
 
-// export default ProjectsList;
+export default ProjectsList;
